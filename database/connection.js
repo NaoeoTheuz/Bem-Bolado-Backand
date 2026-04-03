2c9f4e7467a0bffe7518f257addba432f504ae67
Mhzin <matheushgoliver@gmail.com>
2026-04-02 21:10:55 -0300

    Refactor database connection and add model associations
    Added models and associations for User and Post.

--- database/connection.js
+++ database/connection.js
@@ -17,3 +17,32 @@ const sequelize = new Sequelize(
 );
 
 module.exports = sequelize;
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: 3306,
+        logging: false,
+        dialectOptions: {
+            connectTimeout: 60000
+        }
+    }
+);
+
+// Importar modelos
+const User = require('../models/User');
+const Post = require('../models/Post');
+const Like = require('../models/Like');
+const SavedPost = require('../models/SavedPost');
+
+// Configurar associações
+User.associate({ Post });
+Post.associate({ User });
+
+module.exports = sequelize;
