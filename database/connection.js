ff3070d9243e728db982fbf77ca1fc11783e2b87
Mhzin <matheushgoliver@gmail.com>
2026-04-07 19:54:57 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -8,29 +8,14 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
-        logging: false,
-        define: {
-            timestamps: true,
-            underscored: true
-        }
-    }
-);
-
-module.exports = sequelize;
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
         port: 3306,
         logging: false,
         dialectOptions: {
             connectTimeout: 60000
+        },
+        define: {
+            timestamps: true,
+            underscored: true
         }
     }
 );
@@ -42,7 +27,9 @@ const Like = require('../models/Like');
 const SavedPost = require('../models/SavedPost');
 
 // Configurar associações
-User.associate({ Post });
-Post.associate({ User });
+if (User.associate) User.associate({ Post, Like, SavedPost });
+if (Post.associate) Post.associate({ User, Like, SavedPost });
+if (Like && Like.associate) Like.associate({ User, Post });
+if (SavedPost && SavedPost.associate) SavedPost.associate({ User, Post });
 
 module.exports = sequelize;
