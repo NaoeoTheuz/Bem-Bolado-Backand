b6c7b228b3c9641374a4f6fe8757d1aead6392dc
Mhzin <matheushgoliver@gmail.com>
2026-04-14 21:16:01 -0300

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -25,34 +25,6 @@ const Post = sequelize.define('Post', {
     }
 });
 
-module.exports = Post;
-const { DataTypes } = require('sequelize');
-const sequelize = require('../database/connection');
-
-const Post = sequelize.define('Post', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    usuario_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    imagem: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    descricao: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    hashtag: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
-
 // ASSOCIAÇÕES
 Post.associate = (models) => {
     Post.belongsTo(models.User, { foreignKey: 'usuario_id' });
